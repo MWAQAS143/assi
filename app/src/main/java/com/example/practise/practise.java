15098177b43bf12282d89877b51aece3ec8d3a79
muhammad waqas <mcsm-s21-006@superior.edu.pk>
2022-02-21 10:40:11 +0500

    initial commit

---
+++ app/src/main/java/com/example/practise/practise.java
@@ -0,0 +1,14 @@
+package com.example.practise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class practise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practise);
+    }
+}
